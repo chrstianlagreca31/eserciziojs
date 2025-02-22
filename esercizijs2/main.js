8cdeac242f2be7abfb6b47ac936f2fabcd22d183
chrstianlagreca31 <chris06lagreca@gmail.com>
2025-02-22 13:28:43 +0100

    Add files via upload

---
+++ esercizijs2/main.js
@@ -0,0 +1,14 @@
+let num1 = 5;
+let num2 = 10;
+let num3 = 15;
+let num4 = 20;
+let num5 = 25;
+
+
+let somma = num1 + num2 + num3 + num4 + num5;
+
+
+let media = somma / 5;
+
+
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
