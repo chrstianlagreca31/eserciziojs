65f5ebb7d61cf63062290ad3e695e8049a528370
chrstianlagreca31 <chris06lagreca@gmail.com>
2025-02-22 09:35:41 +0100

    Add files via upload

---
+++ esercizijs/main.js
@@ -0,0 +1,14 @@
+let num1 = 2;
+let num2 = 4;
+alert('il valore del numero 1 è 2');
+alert('il valore del numero 2 è 4');
+let stringa = ('ciaooo');
+alert ('il valore di stringa è ciaooo')
+stringa = ('ciao2');
+console.log(stringa);
+console.log(num1);
+console.log(num2);
+const pgreco = 3.14159265359;
+alert('il valore di pgreco è 3.14159265359');
+console.log(pgreco);
+pgreco = 4;
