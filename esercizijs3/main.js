45f455c4c679540cc999d4f759d23f552b18e321
chrstianlagreca31 <chris06lagreca@gmail.com>
2025-02-22 13:36:56 +0100

    Add files via upload

---
+++ esercizijs3/main.js
@@ -0,0 +1,7 @@
+let annoCorrente = 2025; 
+let annoNascita = 1990; 
+
+let eta = annoCorrente - annoNascita;
+let anniMancanti = 100 - eta;
+
+console.log(`Hai ${eta} anni e ti mancano ${anniMancanti} anni per compierne 100.`);
